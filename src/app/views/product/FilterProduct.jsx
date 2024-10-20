9e31c7e3ebd27609eb2f118e1c0404d032e42bc1
retro1000 <damithajayawardana678@gmail.com>
2024-10-20 23:41:07 +0530

    update filtering and pagination of products

--- src/app/views/product/FilterProduct.jsx
+++ src/app/views/product/FilterProduct.jsx
@@ -4,215 +4,364 @@ import { Box, Typography, Button, Grid, Container } from "@mui/material";
 import { Header, Footer } from "app/components";
 import SortButton from "./component/SortButton";
 import SearchBar from "./component/SearchBar";
-import { FilterList } from "@mui/icons-material";
 import ProductGrid from "./component/ProductGrid";
-import FilterBar, { filtersConfig } from "./component/Filtering";
+import FilterBar from "./component/Filtering";
+import { useEffect } from "react";
+import { useAxios } from "app/hooks/useAxios";
+import { useLocation } from "react-router-dom";
+import { useReducer } from "react";
+import { useNotistack } from "app/hooks/useNotistack";
+import { min } from "lodash";
+
+
+const demoData = [
+  {
+    id: 1,
+    name: "Dummy Paint",
+    price: "$360",
+    rating: 4.5,
+    reviews: 95,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: true
+  },
+  {
+    id: 2,
+    name: "Dummy Paint",
+    price: "$700",
+    rating: 4.8,
+    reviews: 325,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  },
+  {
+    id: 3,
+    name: "Dummy Paint Dummy Paint Dummy Paint Dummy Paint",
+    price: "LKR 5990.00",
+    rating: 4.7,
+    reviews: 145,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: true,
+    isNew: true,
+    realPrice: 'LKR 6990.00'
+
+  },
+  {
+    id: 4,
+    name: "dummy paint",
+    price: "$1160",
+    rating: 4.0,
+    reviews: 35,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  },
+  {
+    id: 5,
+    name: "dummy paint",
+    price: "$1160",
+    rating: 4.0,
+    reviews: 35,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  }, {
+    id: 6,
+    name: "dummy paint",
+    price: "$1160",
+    rating: 4.0,
+    reviews: 35,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  }, {
+    id: 7,
+    name: "dummy paint",
+    price: "$1160",
+    rating: 4.0,
+    reviews: 35,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  }, {
+    id: 8,
+    name: "dummy paint",
+    price: "$1160",
+    rating: 4.0,
+    reviews: 35,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  }, {
+    id: 9,
+    name: "dummy paint",
+    price: "$1160",
+    rating: 4.0,
+    reviews: 35,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  }, {
+    id: 10,
+    name: "dummy paint",
+    price: "$1160",
+    rating: 4.0,
+    reviews: 35,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  }, {
+    id: 11,
+    name: "dummy paint",
+    price: "$1160",
+    rating: 4.0,
+    reviews: 35,
+    imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
+    wishList: false
+
+  },
+]
+
+// const filters = {
+//   positionExterior: false,
+//   positionInterior: false,
+//   positionInteriorandExterior: false,
+//   producttypeCleaner: false,
+//   producttypePaint: false,
+//   producttypeUndercoat: false,
+//   producttypeVarnish: false,
+//   producttypeWaterproofing: false,
+//   roomtypesBathroom: false,
+//   roomtypesBedroom: false,
+//   roomtypesChildrensRoom: false,
+//   roomtypesDiningRoom: false,
+//   roomtypesHallway: false,
+//   roomtypesHomeOffice: false,
+//   roomtypesKitchen: false,
+//   roomtypesLivingRoom: false,
+//   surfaceBluestone: false,
+//   surfaceDoors: false,
+//   surfaceFurniture: false,
+//   surfaceMetal: false,
+//   surfaceWalls: false,
+//   surfaceWindows: false,
+//   surfaceWood: false,
+//   finishGloss: false,
+//   finishGlossSemiGlossMatt: false,
+//   finishHighGloss: false,
+//   finishLowSheen: false,
+//   finishMatt: false,
+//   finishMidSheen: false,
+//   finishNA: false,
+//   finishSemiGloss: false,
+// }
+
+const filterConfig = [
+  
+  {
+    category: 'Delivery options and offers',
+    options: ['Free delivery', 'Sale', 'Discount'],
+    type: 'check'
+  },
+  {
+    category: 'Order types',
+    options: ['All', 'Backend orders'],
+    type: 'radio'
+  },
+  {
+    category: 'Price',
+    config: {label: 'Price', min: 50, max: 15000},
+    type: 'num_slider'
+  }
+]
+
+const initialState = {
+  // filteredProducts: [],
+  filteredProducts: demoData,
+  filters: filterConfig,
+  selectedFilters: {},
+  page: 1,
+  limit: 100,
+  init: false,
+  actionType: 'page',
+  sort: "POPULAR",
+  isPagingBlock: false,
+  // totalResults: 0
+  totalResults: 1000
+}
+
+const filterReducer = (state, action) => {
+
+  switch(action.type){
+    case "FILTER": {
+      const { filteredProducts, totalResults, filters } = action.payload
+      return {...state, page: 1, filteredProducts: filteredProducts, totalResults: totalResults, actionType: 'page', filters: filters?[...state.filters, ...filters]:state.filters, isPagingBlock: false };
+    }case "SELECT_FILTER": {
+      const { selectedFilters } = action.payload
+      return {...state, page: 1, selectedFilters: selectedFilters || {}, actionType: 'filter', isPagingBlock: false };
+    }case "INIT": {
+      const { page, limit, sort, ...filters } = action.payload
+      return {...state, page: page || 1, limit: limit || 100, selectedFilters: filters || {}, init: true, actionType: 'page', sort: sort || state.sort, isPagingBlock: false };
+    }case "PAGE": {
+      const { filteredProducts, totalResults, filters, isPagingBlock } = action.payload
+      return {...state, filteredProducts: [...state.filteredProducts, ...filteredProducts], totalResults: totalResults, actionType: 'page', filters: filters?[...state.filters, ...filters]:state.filters, isPagingBlock: isPagingBlock };
+    }case "SET_PAGE": {
+      return {...state, page: state.isPagingBlock?state.page:(parseInt(state.page, 10)+1), actionType: 'page', isPagingBlock: false };
+    }case "CLEAR_ALL_FILTERS": {
+      return {...state, selectedFilters: {}, page: 1, actionType: 'page', isPagingBlock: false };
+    }case "SORT": {
+      const { sort } = action.payload
+      return {...state, sort: sort || state.sort, page: 1, actionType: 'sort', isPagingBlock: false };
+    }default: {}
+  }
+}
 
 const ProductPage = () => {
-  const [filters, setFilters] = useState({
-    positionExterior: false,
-    positionInterior: false,
-    positionInteriorandExterior: false,
-    producttypeCleaner: false,
-    producttypePaint: false,
-    producttypeUndercoat: false,
-    producttypeVarnish: false,
-    producttypeWaterproofing: false,
-    roomtypesBathroom: false,
-    roomtypesBedroom: false,
-    roomtypesChildrensRoom: false,
-    roomtypesDiningRoom: false,
-    roomtypesHallway: false,
-    roomtypesHomeOffice: false,
-    roomtypesKitchen: false,
-    roomtypesLivingRoom: false,
-    surfaceBluestone: false,
-    surfaceDoors: false,
-    surfaceFurniture: false,
-    surfaceMetal: false,
-    surfaceWalls: false,
-    surfaceWindows: false,
-    surfaceWood: false,
-    finishGloss: false,
-    finishGlossSemiGlossMatt: false,
-    finishHighGloss: false,
-    finishLowSheen: false,
-    finishMatt: false,
-    finishMidSheen: false,
-    finishNA: false,
-    finishSemiGloss: false,
-  });
 
-  const handleFilterChange = (event) => {
-    setFilters({
-      ...filters,
-      [event.target.name]: event.target.checked,
-    });
+  const [state, dispatch] = useReducer(filterReducer, initialState)
+
+  const [loading, setLoading] = useState(false)
+
+  const { api } = useAxios()
+
+  const { triggerCommonErrors } = useNotistack()
+
+  const getUrlParams = () => {
+    const params = {}
+    const searchParams = window.location.href.split('?')
+    if(!searchParams || searchParams.length===1) return {}
+    searchParams[1].split('&')?.forEach(param => {
+      const [key, value] = param.split('=')
+      const values = value.split(',')
+      if(values) params[key] = values.length===1?values[0]:values
+    })
+    return params
+  }
+
+  useEffect(() => {
+    dispatch({ type: "INIT", payload: getUrlParams() })
+    const pageEl = document.getElementById("@content_box")
+    if(pageEl){
+      pageEl.addEventListener("scroll", handleScroll);
+      return () => pageEl.removeEventListener("scroll", handleScroll);
+    }
+  }, [])
+
+  const handleFilterChange = (value) => {
+    dispatch({ type: "SELECT_FILTER", payload: {selectedFilters: value} })
   };
 
   const handleClearAll = () => {
-    const resetFilters = {};
-    Object.keys(filters).forEach((key) => {
-      resetFilters[key] = false;
-    });
-    setFilters(resetFilters);
+    dispatch({ type: "CLEAR_ALL_FILTERS", payload: {} })
   };
 
-  const [filteredProducts, setFilteredProducts] = useState(
-    [
-      {
-        id: 1,
-        name: "Dummy Paint",
-        price: "$360",
-        rating: 4.5,
-        reviews: 95,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: true
-      },
-      {
-        id: 2,
-        name: "Dummy Paint",
-        price: "$700",
-        rating: 4.8,
-        reviews: 325,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      },
-      {
-        id: 3,
-        name: "Dummy Paint Dummy Paint Dummy Paint Dummy Paint",
-        price: "LKR 5990.00",
-        rating: 4.7,
-        reviews: 145,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: true,
-        isNew: true,
-        realPrice: 'LKR 6990.00'
-    
-      },
-      {
-        id: 4,
-        name: "dummy paint",
-        price: "$1160",
-        rating: 4.0,
-        reviews: 35,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      },
-      {
-        id: 5,
-        name: "dummy paint",
-        price: "$1160",
-        rating: 4.0,
-        reviews: 35,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      }, {
-        id: 6,
-        name: "dummy paint",
-        price: "$1160",
-        rating: 4.0,
-        reviews: 35,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      }, {
-        id: 7,
-        name: "dummy paint",
-        price: "$1160",
-        rating: 4.0,
-        reviews: 35,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      }, {
-        id: 8,
-        name: "dummy paint",
-        price: "$1160",
-        rating: 4.0,
-        reviews: 35,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      }, {
-        id: 9,
-        name: "dummy paint",
-        price: "$1160",
-        rating: 4.0,
-        reviews: 35,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      }, {
-        id: 10,
-        name: "dummy paint",
-        price: "$1160",
-        rating: 4.0,
-        reviews: 35,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      }, {
-        id: 11,
-        name: "dummy paint",
-        price: "$1160",
-        rating: 4.0,
-        reviews: 35,
-        imgs: ['/assets/images/2099.jpg', '/assets/images/8735.jpg', '/assets/images/amazon-2.png', '/assets/images/demo_home_two.jpg'],
-        wishList: false
-    
-      },
-    ]
-  )
+  const handleSort = (value) => {
+    dispatch({ type: "SORT", payload: { sort: value } })
+  } 
+
+  const handleScroll = () => {
+    const pageEl = document.getElementById("@content_box")
+    if(pageEl){
+      const { scrollHeight, scrollTop, clientHeight } = pageEl;
+
+      scrollTop + clientHeight >= scrollHeight && 
+      !loading &&
+      (state.totalResults === 0 || state.filteredProducts.length < state.totalResults) && 
+      !state.isPagingBlock && 
+      dispatch({ type: "SET_PAGE", payload: {} })
+    }
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    !state.isPagingBlock && state.init && fetchData(controller)
+    return () => {
+      controller.abort();
+    };
+  }, [state.page, state.selectedFilters, state.init, state.sort]);
+
+  const generateFilterUrl = () => {
+    const selectedFilters = state.selectedFilters
+    return `/product/filter-product?page=${state.page}&limit=${state.limit}&sort=${state.sort}${
+      selectedFilters && Object.keys(selectedFilters).length>0 ? '&'+Object.keys(selectedFilters).map(key => key+'='+(Array.isArray(selectedFilters[key])?selectedFilters[key].join(','):selectedFilters[key])).join('&') : ''
+    }`;
+  } 
+
+  const fetchData = async (controller) => {
+    console.log(state)
+    setLoading(true)
+    const filterUrl = generateFilterUrl();
+    window.history.pushState({}, '', filterUrl)
+    await api.get(filterUrl, {signal: controller.signal})
+      .then(response => {
+        if(response.status===200){
+          // dispatch({ type: state.filter ? "FILTER" : "PAGE", payload: {...response.data} })
+          dispatch({ type: state.actionType==='filter' ? "FILTER" : state.actionType==='page' ? "PAGE" : "SORT", payload: {filteredProducts: [...state.filteredProducts, ...state.filteredProducts], totalResults: 1000, isPagingBlock: state.actionType!=='page' } })
+          if(response.status===204) dispatch({ type: state.actionType==='filter' ? "FILTER" : state.actionType==='page' ? "PAGE" : "SORT", payload: {filteredProducts: [], totalResults: state.totalResults, isPagingBlock: true} })
+        }
+      })
+      .catch(error => {
+        triggerCommonErrors(error)
+        dispatch({ type: state.actionType==='filter' ? "FILTER" : state.actionType==='page' ? "PAGE" : "SORT", payload: {filteredProducts: [], totalResults: state.totalResults, isPagingBlock: true} })
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  };
 
   return (
       
     <Box>
-      <Container sx={{display: 'flex', flexDirection: 'column'}}>
-        {/* <Grid container spacing={3}> */}
-            <br />
-            <Header title="Our Products" subTitle="Explore Our Products"/>
-            <Box 
-              sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'flex-end',
-                alignItems: 'flex-start',
-                gap: 2,
-                marginTop: '2em',
-                flexWrap: 'wrap'
-              }}
-            >
-              <SortButton/>
-              <SearchBar/>
+      <Container sx={{ display: 'flex', flexDirection: 'column' }} maxWidth={'1400px'}>
+        <br />
+        <Header title="Our Products" subTitle="Explore Our Products" />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            alignItems: 'flex-start',
+            gap: 2,
+            marginTop: '2em',
+            flexWrap: 'wrap',
+          }}
+        >
+          <SortButton sort={state.sort} handleSort={handleSort} />
+        </Box>
+        <Grid
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+            gap: '1em',
+            marginTop: '2em',
+            width: '100dvw',
+            minHeight: '100dvh',
+          }}
+        >
+          {/* Filters section */}
+          <Box sx={{ position: 'sticky', top: 0, width: '20%', maxHeight: '100vh', overflowY: 'auto'}}>
+            <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+              <Typography variant="h5" gutterBottom>
+                Filters
+              </Typography>
+              <Button variant="text" color="primary" onClick={handleClearAll} disabled={Object.keys(state.selectedFilters).length===0}>
+                Clear All
+              </Button>
             </Box>
-            <Grid sx={{display: 'flex', justifyContent: 'center', alignItems: 'flex-start', gap:'1em', marginTop: '2em'}}>
-              <Box sx={{ position: 'sticky', width: '30%', mr: 2 }}>
-                <Typography variant="h5" gutterBottom>
-                  Filters
-                </Typography>
-                <Button variant="text" color="primary" onClick={handleClearAll}>
-                  Clear All
-                </Button>
-                {/* <FilterList
-                  filters={filters}
-                  filtersConfig={filtersConfig}
-                  handleFilterChange={handleFilterChange}
-                /> */}
-                <FilterBar filters={filters} handleFilterChange={handleFilterChange} handleClearAll={handleClearAll}/>
-              </Box>
-              <Box display={'flex'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'} padding={5}>
-                <ProductGrid products={filteredProducts}/>
-              </Box>
-            </Grid>
-        {/* </Grid> */}
-      </Container>
-      <Footer />
+            <FilterBar filters={state.filters} handleFilterChange={handleFilterChange} selectedFilters={state.selectedFilters} />
+          </Box>
 
+          {/* Products section */}
+          <Box display={'flex'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'} padding={5} width={'100%'}>
+            <ProductGrid products={state.filteredProducts} sx={{ maxWidth: '100%' }} maxWidth={'100%'}/>
+          </Box>
+        </Grid>
+      </Container>
+      <br></br>
+      <Footer />
     </Box>
+
   );
 };
 
--- src/app/views/product/component/Filtering.jsx
+++ src/app/views/product/component/Filtering.jsx
@@ -1,70 +1,145 @@
 // FilterBar.jsx
 import React, { useState } from 'react';
-import { Box, Typography, FormGroup, FormControlLabel, Checkbox, Button, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import { Box, Typography, FormGroup, FormControlLabel, Checkbox, Accordion, AccordionSummary, AccordionDetails, Radio } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { NumSliderFilter } from 'app/components';
 
-export const filtersConfig = [
-  {
-    category: 'Position',
-    options: ['Exterior', 'Interior', 'Interior and Exterior'],
-  },
-  {
-    category: 'Product type',
-    options: ['Cleaner', 'Paint', 'Undercoat', 'Varnish', 'Waterproofing'],
-  },
-  {
-    category: 'Room types',
-    options: [
-      'Bathroom',
-      'Bedroom',
-      "Children's Room",
-      'Dining Room',
-      'Hallway',
-      'Home Office',
-      'Kitchen',
-      'Living Room',
-    ],
-  },
-  {
-    category: 'Surface',
-    options: ['Bluestone', 'Doors', 'Furniture', 'Metal', 'Walls', 'Windows', 'Wood'],
-  },
-  {
-    category: 'Finish',
-    options: ['Gloss', 'Gloss, Semi Gloss Matt', 'High Gloss', 'Low Sheen', 'Matt', 'Mid Sheen', 'NA', 'Semi Gloss'],
-  },
-];
+const handleCheckChange = (e, filter, selectedFilters, setSelectedFilters) => {
+  const { name, checked } = e.target;
+  const category = filter.category.toLowerCase().replace(/ /g, '');
 
-const FilterList = ({ filter, filters, handleFilterChange }) => (
-  <Accordion sx={{ boxShadow: 'none', border: 'none' }}>
-    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-      <Typography variant="h6">{filter.category}</Typography>
-    </AccordionSummary>
-    <AccordionDetails>
-      <FormGroup>
-        {filter.options.map((option, index) => (
-          <FormControlLabel
-            key={index}
-            control={
-              <Checkbox
-                checked={filters[`${filter.category.toLowerCase().replace(/ /g, '')}${option.replace(/ /g, '')}`] || false}
-                onChange={handleFilterChange}
-                name={`${filter.category.toLowerCase().replace(/ /g, '')}${option.replace(/ /g, '')}`}
+  // Extract the selected options for the category
+  const selectedOptions = selectedFilters[category] || [];
+console.log(selectedOptions)
+  if (checked) {
+    // Add the option to the selected array
+    setSelectedFilters({
+      ...selectedFilters,
+      [category]: Array.isArray(selectedOptions) ? [...selectedOptions, name] : [name],
+    });
+  } else {
+    // Remove the option from the selected array
+    const updatedOptions = selectedOptions.filter((option) => option !== name);
+
+    const newFilters = { ...selectedFilters };
+
+    if (updatedOptions.length > 0) {
+      newFilters[category] = updatedOptions;
+    } else {
+      delete newFilters[category];
+    }
+
+    setSelectedFilters(newFilters);
+  }
+};
+
+const handleRadioChange = (e, filter, selectedFilters, setSelectedFilters) => {
+  const { value } = e.target;
+  const category = filter.category.toLowerCase().replace(/ /g, '');
+
+  // Update the selected radio option for the category
+  setSelectedFilters({
+    ...selectedFilters,
+    [category]: value,
+  });
+};
+
+const handleNumSliderChange = (newRange, filter, selectedFilters, setSelectedFilters) => {
+  const category = filter.category.toLowerCase().replace(/ /g, '');
+
+  // Update the slider range in the selected filters
+  setSelectedFilters({
+    ...selectedFilters,
+    [category]: newRange,
+  });
+};
+
+const createFilterOption = (filter, filters, handleFilterChange) => {
+  switch(filter.type){
+    case 'check':
+      return (
+        <FormGroup>
+          {filter.options.map((option, index) => (
+              <FormControlLabel
+                key={index}
+                control={
+                  <Checkbox
+                    size="small"
+                    checked={Array.isArray(filters[`${filter.category.toLowerCase().replace(/ /g, '')}`]) && filters[`${filter.category.toLowerCase().replace(/ /g, '')}`].includes(option.replace(/ /g, ''))}
+                    onChange={(e) => handleCheckChange(e, filter, filters, handleFilterChange)}
+                    name={option.replace(/ /g, '')}
+                  />
+                }
+                label={option}
+                sx={{
+                  marginBottom: '-10px',
+                  marginLeft: '1px',
+                  '.MuiFormControlLabel-label': { fontSize: '14px' },
+                }}
               />
-            }
-            label={option}
-          />
-        ))}
-      </FormGroup>
+          ))}
+        </FormGroup>
+      )
+    case 'radio':
+      return (
+        <FormGroup>
+          {filter.options.map((option, index) => (
+            <FormControlLabel
+              key={index}
+              control={
+                <Radio
+                  size='small'
+                  checked={filters[`${filter.category.toLowerCase().replace(/ /g, '')}`] === option}
+                  onChange={(e) => handleRadioChange(e, filter, filters, handleFilterChange)}
+                  value={option}
+                  name={filter.category.toLowerCase().replace(/ /g, '')} // Ensure name is the same for grouping
+                />
+              }
+              label={option}
+              sx={{
+                marginLeft: '1px',
+                marginBottom: '-10px',
+                '.MuiFormControlLabel-label': { // Targeting the label
+                  fontSize: '14px',
+                },
+              }}
+            />
+          ))}
+        </FormGroup>
+      )
+    case 'num_slider':
+      return (
+        <NumSliderFilter 
+          range={filters[`${filter.category.toLowerCase().replace(/ /g, '')}`]} 
+          curr={'LKR'} 
+          setRange={(newRange) => handleNumSliderChange(newRange, filter, filters, handleFilterChange)}
+          min={10} 
+          max={30000} 
+          {...filter.config}
+          minWidth={100}
+        />
+      )
+    default: return ''
+  }
+}
+
+const FilterList = ({ key, filter, handleFilterChange, selectedFilters }) => (
+  <Accordion defaultExpanded key={key} sx={{ boxShadow: 'none', border: 'none', borderBottom: '1px solid silver', margin: '1px !important'}}>
+    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+      <Typography variant="body1">{filter.category}</Typography>
+    </AccordionSummary>
+    <AccordionDetails sx={{mt: -2, pb: 1}}>
+      {createFilterOption(filter, selectedFilters, handleFilterChange)}
+      <br></br>
     </AccordionDetails>
   </Accordion>
 );
 
-const FilterBar = ({ filters, handleFilterChange, handleClearAll }) => {
+const FilterBar = ({ filters, handleFilterChange, selectedFilters }) => {
   return (
     <>
-      {filtersConfig.map((filter, index) => (
-        <FilterList key={index} filter={filter} filters={filters} handleFilterChange={handleFilterChange} />
+      {filters.map((filter, index) => (
+        <FilterList key={index} filter={filter} handleFilterChange={handleFilterChange} selectedFilters={selectedFilters} />
       ))}
     </>
   );
